8b98d91323aab8a56c0e024346ff8456b250fd42
Mark van Beek <flipvb@gmail.com>
2018-01-12 11:59:58 +0100

    [FIX] Error: Cannot find module '../cordova/platform_metadata'

--- scripts/create_strings.js
+++ scripts/create_strings.js
@@ -2,21 +2,18 @@ var ios_script = require('./create_ios_strings');
 var android_script = require('./create_android_strings');
 
 module.exports = function(context) {
-  var Q = context.requireCordovaModule('q')
-  var platformMetadata = context.requireCordovaModule('cordova-lib/src/cordova/platform_metadata');
+    var Q = context.requireCordovaModule('q');
+    var platforms = context.requireCordovaModule('cordova-lib/src/cordova/util').listPlatforms(context.opts.projectRoot);
 
-  return platformMetadata.getPlatformVersions(context.opts.projectRoot)
-    .then(function(platformVersions) {
-      var promises = [];
-      var platforms = platformVersions.map(function(platformVersion) {
-        return platformVersion.platform;
-      });
-      if (platforms.indexOf('ios') >= 0) {
+    var promises = [];
+
+    if (platforms.indexOf('ios') >= 0) {
         promises.push(ios_script(context));
-      }
-      if (platforms.indexOf('android') >= 0) {
+    }
+
+    if (platforms.indexOf('android') >= 0) {
         promises.push(android_script(context));
-      }
-      return Q.all(promises);
-    });
-}
+    }
+
+    return Q.all(promises);
+};
