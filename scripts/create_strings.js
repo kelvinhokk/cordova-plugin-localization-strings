832b125386e99e30cb18fb88a84af72e2601f2c8
shelart <shelekhov.arthur@gmail.com>
2018-05-12 19:53:44 +0300

    Fix bug with attempting to handle a platform while building another one

--- scripts/create_strings.js
+++ scripts/create_strings.js
@@ -3,7 +3,7 @@ var android_script = require('./create_android_strings');
 
 module.exports = function(context) {
     var Q = require('q');
-    var platforms = context.requireCordovaModule('cordova-lib/src/cordova/util').listPlatforms(context.opts.projectRoot);
+    var platforms = context.opts.platforms;
 
     var promises = [];
 
@@ -14,6 +14,16 @@ module.exports = function(context) {
     if (platforms.indexOf('android') >= 0) {
         promises.push(android_script(context));
     }
+
+    // https://stackoverflow.com/a/43994999/2175025
+    process.on('unhandledRejection', function(reason, p) {
+        console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+        // application specific logging, throwing an error, or other logic here
+    });
+    console.log('Listening to promises rejection');
+
+    console.log('Promises');
+    console.log(promises);
 
     return Q.all(promises);
 };
