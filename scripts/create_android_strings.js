9e73ee65e671490250f7da7a17b7502894e59394
JanMisker <jan@misker.nl>
2018-12-18 11:43:23 +0100

    Update create_android_strings.js

--- scripts/create_android_strings.js
+++ scripts/create_android_strings.js
@@ -165,7 +165,7 @@ function processResult(context, lang, langJson, stringXmlJson) {
         }
     });
 
-    var langJsonToProcess = _.assignIn(langJson.config_android, langJson.app);
+    var langJsonToProcess = _.assignIn(langJson.config_android, langJson.app_android);
 
     //now iterate through langJsonToProcess
     _.forEach(langJsonToProcess, function (val, key) {
